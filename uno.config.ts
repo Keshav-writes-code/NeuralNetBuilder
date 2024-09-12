6ee0691bc8e3ae40ea273558e3a543432fd6f849
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-09-13 00:30:17 +0530

    add variant group

--- uno.config.ts
+++ uno.config.ts
@@ -2,10 +2,14 @@
 import { defineConfig } from 'unocss'
 import { presetUno } from 'unocss'
 import {presetDaisy} from 'unocss-preset-daisy'
+import transformerVariantGroup from '@unocss/transformer-variant-group'
 
 export default defineConfig({
   presets: [
     presetUno(),
     presetDaisy()
+  ],
+  transformers: [
+    transformerVariantGroup(),
   ],
 })

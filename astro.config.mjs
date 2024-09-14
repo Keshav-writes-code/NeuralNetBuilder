fd116a69d8f87e1282ff24fcc97ca6baeec4da92
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-09-14 17:23:29 +0530

    add: tailwind and DaisyUI for Theme Switching Capability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // astro.config.ts
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
-
 import svelte from '@astrojs/svelte';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
     integrations: [
         UnoCSS({
             injectReset: true // or a path to the reset file
         }),
-        svelte()
+        svelte(),
+        tailwind()
     ],
     site: 'https://Keshav-writes-code.github.io',
     base: 'NeuralNetBuilder',    

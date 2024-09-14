e0fc58c7ca70acee56600f8fd156b6ce6c7e9296
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-09-14 17:26:20 +0530

    code formatting

--- uno.config.ts
+++ uno.config.ts
@@ -1,7 +1,7 @@
 // uno.config.ts
 import { defineConfig } from 'unocss'
 import { presetUno } from 'unocss'
-import {presetDaisy} from 'unocss-preset-daisy'
+import { presetDaisy } from 'unocss-preset-daisy'
 import transformerVariantGroup from '@unocss/transformer-variant-group'
 
 export default defineConfig({

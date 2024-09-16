6ae3ce64e570c97fb3aedb852332894eb7e77be4
keshav-writes-code <dhimankeshav201@gmail.com>
2024-09-16 19:40:51 +0530

    change display slider Limits, better layout of parameter changing box, added = sign, scroll if more than 4 wieght sliders are there

--- src/components/NNBuilder_component.svelte
+++ src/components/NNBuilder_component.svelte
@@ -167,7 +167,7 @@
           </div>
         </div>
 
-        <label class="form-control">
+        <label class="form-control ">
           <div class="label">
             <span class="label-text">Bias</span>
             <span class="label-text-alt"
@@ -175,7 +175,7 @@
                 currentNeuron.idy
               ].bias}</span
             >
-            <span class="label-text-alt">-10 to 10</span>
+            <span class="label-text-alt">-1 to 1</span>
           </div>
           <input
             type="range"
@@ -191,10 +191,10 @@
       {:else}
         <h1 class="text-2xl">Select a Neuron</h1>
         <label class="form-control opacity-50">
-          <div class="label">
+          <div class="label grid grid-cols-[1fr_auto_1fr] ">
             <span class="label-text">Bias</span>
-            <span class="label-text-alt">Value Nothing</span>
-            <span class="label-text-alt">-∞ to ∞</span>
+            <span class="label-text-alt">Value = Nothing</span>
+            <span class="label-text-alt justify-self-end">-∞ to ∞</span>
           </div>
           <input
             type="range"
@@ -208,47 +208,50 @@
       {/if}
 
       <div class="divider"></div>
-      {#if currentNeuron}
-        {#each hidOutLayers[currentNeuron.idx].neurons[currentNeuron.idy].weights as _, i}
-          <label class="form-control">
-            <div class="label">
-              <span class="label-text">Weight {i + 1}</span>
-              <span class="label-text-alt"
-                >Value = {hidOutLayers[currentNeuron.idx].neurons[
-                  currentNeuron.idy
-                ].weights[i]}</span
-              >
-              <span class="label-text-alt">-10 to 10</span>
-            </div>
-            <input
-              type="range"
-              min="-1"
-              max="1"
-              step="0.01"
-              class="range range-lg w-full"
-              bind:value={hidOutLayers[currentNeuron.idx].neurons[
-                currentNeuron.idy
-              ].weights[i]}
-            />
-          </label>
-        {/each}
-      {:else}
-        <label class="form-control opacity-50">
-          <div class="label">
-            <span class="label-text">Weight None</span>
-            <span class="label-text-alt">Value = Nothing</span>
-            <span class="label-text-alt">-∞ to ∞</span>
-          </div>
-          <input
-            type="range"
-            min="-10"
-            max="10"
-            step="0.01"
-            class="range range-lg w-full"
-            disabled
-          />
-        </label>
-      {/if}
+      <div class="max-h-300px overflow-y-auto">
+          {#if currentNeuron}
+            {#each hidOutLayers[currentNeuron.idx].neurons[currentNeuron.idy].weights as _, i}
+              <label class="form-control">
+                <div class="label">
+                  <span class="label-text">Weight {i + 1}</span>
+                  <span class="label-text-alt"
+                    >Value = {hidOutLayers[currentNeuron.idx].neurons[
+                      currentNeuron.idy
+                    ].weights[i]}</span
+                  >
+                  <span class="label-text-alt">-1 to 1</span>
+                </div>
+                <input
+                  type="range"
+                  min="-1"
+                  max="1"
+                  step="0.01"
+                  class="range range-lg w-full"
+                  bind:value={hidOutLayers[currentNeuron.idx].neurons[
+                    currentNeuron.idy
+                  ].weights[i]}
+                />
+              </label>
+            {/each}
+          {:else}
+            <label class="form-control opacity-50">
+              <div class="label grid grid-cols-[1fr_auto_1fr] ">
+                <span class="label-text">Weight None</span>
+                <span class="label-text-alt">Value = Nothing</span>
+                <span class="label-text-alt justify-self-end">-∞ to ∞</span>
+              </div>
+              <input
+                type="range"
+                min="-10"
+                max="10"
+                step="0.01"
+                class="range range-lg w-full"
+                disabled
+              />
+            </label>
+          {/if}
+
+      </div>
     </div>
     <div class=" w-full shadow-xl">
       <canvas class="w-full" id="functionChart"></canvas>

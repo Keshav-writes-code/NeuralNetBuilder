71118f08be7d47af3a11e43417969ddd537f81b9
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-09-17 20:39:07 +0530

    refactor: Extract xy Graph in a component

---
+++ src/components/NN_IOgraph.svelte
@@ -0,0 +1,160 @@
+<script lang="ts">
+  // Libs Import
+  import { onMount } from "svelte";
+  import { Chart } from "chart.js/auto";
+
+  import { Layer, af_enum } from "./NN_classes.ts";
+  import {
+    hidOutLayers_store,
+    selActivaFn_store,
+    hiddenLayersNeuronCount_store,
+    hiddenLayersCount_store,
+  } from "./store.ts";
+
+  //---------------------------------------------
+  // --------------Some Variable-----------------
+  //---------------------------------------------
+
+  const activaFn = {
+    [af_enum.relu]: (x: number) => Math.max(0, x),
+    [af_enum.sigmoid]: (x: number) => 1 / (1 + Math.exp(-x)),
+    [af_enum.tanh]: (x: number) => Math.tanh(x),
+  };
+
+  let selActivaFn: af_enum;
+  selActivaFn_store.subscribe((value) => {
+    selActivaFn = value;
+  });
+
+  let hidOutLayers: Layer[];
+  hidOutLayers_store.subscribe((value) => {
+    hidOutLayers = value;
+  });
+
+  let hiddenLayersNeuronCount: number[] = [1];
+  hiddenLayersNeuronCount_store.subscribe((value) => {
+    hiddenLayersNeuronCount = value;
+  });
+  let hiddenLayersCount = 1;
+  hiddenLayersCount_store.subscribe((value) => {
+    hiddenLayersCount = value;
+  })
+  //---------------------------------------------
+  // Setting up the Input, Hidden & Output Layers
+  //---------------------------------------------
+
+  // Hidden Layers
+  let hiddenLayers: Layer[];
+
+  // I/O Layers
+  let inputLayer = new Layer(1, 0);
+  let outputLayer = new Layer(
+    1,
+    hiddenLayersNeuronCount[hiddenLayersNeuronCount.length - 1]
+  );
+
+  // Hidden & Output Layer Combined
+
+  function neuralNetwork(x: number | string, layers: Layer[], func: Function) {
+    x = parseFloat(x.toString());
+    let inputs = [x];
+
+    for (let i = 0; i < layers.length - 1; i++) {
+      const layer = layers[i];
+      const outputs = [];
+      for (const neuron of layer.neurons) {
+        const weightedSum =
+          neuron.weights.reduce(
+            (sum, weight, i) => sum + weight * inputs[i],
+            0
+          ) + neuron.bias;
+        let neuronOut = func(weightedSum);
+        outputs.push(neuronOut);
+      }
+      inputs = outputs; // Set inputs for the next layer
+    }
+
+    return inputs.reduce((sum, output) => sum + output, 0); // Summing the final layer's outputs
+  }
+  // ---------------------------------------
+  // -------------- Plotting ---------------
+  // ---------------------------------------
+  let chart: Chart;
+  let xValues: number[] = [];
+  let yValues: number[] = [];
+  const range = 10;
+
+  // Function to generate new y values based on x values
+  function updateYValues(layers: Layer[], func: Function) {
+    yValues = xValues.map((x) => neuralNetwork(x, layers, func));
+  }
+
+  // Generate x values
+  for (let x = -range; x <= range; x += 0.1) {
+    xValues.push(x); // Keep x values as strings for labels
+  }
+
+  onMount(() => {
+    const ctx = (document.getElementById("functionChart") as HTMLCanvasElement)?.getContext("2d");
+    chart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: xValues,
+        datasets: [
+          {
+            label: " f(x) = NeuralNet(x)",
+            data: yValues,
+            borderColor: "rgb(75, 192, 192)",
+            fill: false,
+            tension: 0,
+            pointRadius: 0,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          x: {
+            type: "linear",
+            position: "bottom",
+          },
+          y: {
+            beginAtZero: false,
+          },
+        },
+      },
+    });
+    updateYValues(hidOutLayers, activaFn[selActivaFn]);
+    chart.data.datasets[0].data = yValues;
+    chart.options.animation = false; // disables all animations
+    chart.update();
+  });
+
+  // ---------------------------------------
+  // ---------- Reactivity Stuff -----------
+  // ---------------------------------------
+  function updateNet(
+    hiddenLayersCount: number,
+    hiddenLayersNeuronCount: number[]
+  ) {
+    hiddenLayers = new Array(hiddenLayersCount).fill(0).map((_, i) => {
+      return new Layer(
+        hiddenLayersNeuronCount[i],
+        hiddenLayersNeuronCount[i - 1] || 1
+      );
+    });
+    hidOutLayers_store.set([...hiddenLayers, outputLayer]);
+  }
+  $: updateNet(hiddenLayersCount, hiddenLayersNeuronCount);
+
+  $: {
+    updateYValues(hidOutLayers, activaFn[selActivaFn]);
+    if (chart) {
+      chart.data.datasets[0].data = yValues;
+      chart.update();
+    }
+  }
+</script>
+
+<div class="rounded-lg w-full shadow-xl border-neutral b-2">
+  <canvas class="w-full" id="functionChart"></canvas>
+</div>

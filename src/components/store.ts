b844463ae87370078ab3b4632912e4139bd0676e
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-09-17 20:37:45 +0530

    add shared variables, import enums

--- src/components/store.ts
+++ src/components/store.ts
@@ -1,5 +1,8 @@
 import { writable } from 'svelte/store'
-import { cNeuron, Layer } from './NN_classes';
+import { cNeuron, Layer, af_enum } from './NN_classes';
 
 export let currentNeuron_store = writable<cNeuron | null>(null);
 export let hidOutLayers_store = writable<Layer[]>([]);
+export let hiddenLayersCount_store = writable<number>(1);
+export let hiddenLayersNeuronCount_store = writable<number[]>([1]);
+export let selActivaFn_store = writable<af_enum>(af_enum.relu);

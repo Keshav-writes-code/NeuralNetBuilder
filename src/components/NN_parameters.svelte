b7d18ba88c6f024d40ddfcae02dca76a72cb9009
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-09-17 18:40:17 +0530

    refactor: change variale name to be more specific

--- src/components/NN_parameters.svelte
+++ src/components/NN_parameters.svelte
@@ -1,29 +1,29 @@
 <script>
-  import {currentNeuron, hidOutLayers} from './store.ts'
-  let localhidOutLayers
+  import {currentNeuron_store, hidOutLayers_store} from './store.ts'
+  let hidOutLayers
 
-  hidOutLayers.subscribe(value => {
-    localhidOutLayers = value
+  hidOutLayers_store.subscribe(value => {
+    hidOutLayers = value
   })
 
-  function updateNet(localhidOutLayers){
-    hidOutLayers.set(localhidOutLayers)
+  function updateNet(hidOutLayers){
+    hidOutLayers_store.set(hidOutLayers)
   }
 
-  $ : updateNet(localhidOutLayers)
+  $ : updateNet(hidOutLayers)
   
-  let localcurrentNeuron
-  currentNeuron.subscribe(value => {
-    localcurrentNeuron = value
+  let currentNeuron
+  currentNeuron_store.subscribe(value => {
+    currentNeuron = value
   })
 </script>
 
 <div class="border-neutral rounded-lg  b-2 w-full max-w-xl p-10 shadow-2xl relative h-min">
-    {#if localcurrentNeuron}
+    {#if currentNeuron}
       <div class="text-2xl">
         Neuron
         <div class="badge badge-lg text-xl badge-neutral">
-          {localcurrentNeuron.idx + 1}:{localcurrentNeuron.idy + 1}
+          {currentNeuron.idx + 1}:{currentNeuron.idy + 1}
         </div>
       </div>
 
@@ -31,8 +31,8 @@
         <div class="label">
           <span class="label-text">Bias</span>
           <span class="label-text-alt"
-            >Value = {localhidOutLayers[localcurrentNeuron.idx].neurons[
-              localcurrentNeuron.idy
+            >Value = {hidOutLayers[currentNeuron.idx].neurons[
+              currentNeuron.idy
             ].bias}</span
           >
           <span class="label-text-alt">-1 to 1</span>
@@ -43,8 +43,8 @@
           max="10"
           step="0.01"
           class="range range-lg w-full"
-          bind:value={localhidOutLayers[localcurrentNeuron.idx].neurons[
-            localcurrentNeuron.idy
+          bind:value={hidOutLayers[currentNeuron.idx].neurons[
+            currentNeuron.idy
           ].bias}
         />
       </label>
@@ -69,14 +69,14 @@
 
     <div class="divider"></div>
     <div class="max-h-300px overflow-y-auto">
-        {#if localcurrentNeuron}
-          {#each localhidOutLayers[localcurrentNeuron.idx].neurons[localcurrentNeuron.idy].weights as _, i}
+        {#if currentNeuron}
+          {#each hidOutLayers[currentNeuron.idx].neurons[currentNeuron.idy].weights as _, i}
             <label class="form-control">
               <div class="label">
                 <span class="label-text">Weight {i + 1}</span>
                 <span class="label-text-alt"
-                  >Value = {localhidOutLayers[localcurrentNeuron.idx].neurons[
-                    localcurrentNeuron.idy
+                  >Value = {hidOutLayers[currentNeuron.idx].neurons[
+                    currentNeuron.idy
                   ].weights[i]}</span
                 >
                 <span class="label-text-alt">-1 to 1</span>
@@ -87,8 +87,8 @@
                 max="1"
                 step="0.01"
                 class="range range-lg w-full"
-                bind:value={localhidOutLayers[localcurrentNeuron.idx].neurons[
-                  localcurrentNeuron.idy
+                bind:value={hidOutLayers[currentNeuron.idx].neurons[
+                  currentNeuron.idy
                 ].weights[i]}
               />
             </label>

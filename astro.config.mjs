1e73198296ec4c7130052367e63d4c51dc0f8a73
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-09-17 22:32:24 +0530

    add: home page & Dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,12 @@ import UnoCSS from 'unocss/astro'
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 
+import icon from 'astro-icon';
+
 export default defineConfig({
-    integrations: [
-        UnoCSS({
-            injectReset: true // or a path to the reset file
-        }),
-        svelte(),
-        tailwind()
-    ],
+    integrations: [UnoCSS({
+        injectReset: true // or a path to the reset file
+    }), svelte(), tailwind(), icon()],
     site: 'https://Keshav-writes-code.github.io',
     base: 'NeuralNetBuilder',    
 })

24ba16121dfa7e048970b825259053fc1d367654
keshav-writes-code <dhimankeshav201@gmail.com>
2024-09-19 13:05:32 +0530

    remove tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
 import svelte from "@astrojs/svelte";
-import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
 
 export default defineConfig({
@@ -12,7 +10,6 @@ export default defineConfig({
       injectReset: true,
     }),
     svelte(),
-    // tailwind(),
     icon(),
   ],
   site: "https://Keshav-writes-code.github.io",

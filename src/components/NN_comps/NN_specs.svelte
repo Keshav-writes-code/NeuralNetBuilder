f2e4459ed2be5d876fe5cea1dbbb9301a0eac8ca
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-09-20 20:24:05 +0530

    fix: unwanted right margin

--- src/components/NN_comps/NN_specs.svelte
+++ src/components/NN_comps/NN_specs.svelte
@@ -94,7 +94,7 @@
         <svg width="12px" height="12px" class="inline-block h-2 w-2 fill-current opacity-60" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048" > <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z" ></path> </svg>
       </div>
       <ul
-        class="dropdown-content bg-base-300 rounded-box z-[1] min-w-52 p-2  shadow-2xl"
+        class="dropdown-content bg-base-300 rounded-box z-[1] p-2 shadow-2xl"
       >
         <li class="flex items-center" >
           <input

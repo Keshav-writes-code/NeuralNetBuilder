f6592c7146b30bf7759f05e845aa50945356a91c
keshav-writes-code <dhimankeshav201@gmail.com>
2025-01-23 11:04:58 +0530

    feat: add unocss animation preset

--- uno.config.ts
+++ uno.config.ts
@@ -1,19 +1,20 @@
 // uno.config.ts
-import { defineConfig } from 'unocss'
-import { presetUno } from 'unocss'
-import { presetIcons } from 'unocss'
-import { presetDaisy } from 'unocss-preset-daisy'
-import transformerVariantGroup from '@unocss/transformer-variant-group'
+import { defineConfig } from "unocss";
+import { presetUno } from "unocss";
+import { presetIcons } from "unocss";
+import { presetDaisy } from "unocss-preset-daisy";
+import { presetAnimations } from "unocss-preset-animations";
+import transformerVariantGroup from "@unocss/transformer-variant-group";
 
 export default defineConfig({
   presets: [
     presetUno(),
+    presetAnimations(),
     presetDaisy({
       themes: ["business", "dark", "black", "forest"],
     }),
-    presetIcons()
+    presetIcons(),
   ],
-  transformers: [
-    transformerVariantGroup(),
-  ],
-})
+  transformers: [transformerVariantGroup()],
+});
+

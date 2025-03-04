8f66c3df83acc2ca0521e1939a4ad5fb935bd7ec
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-03-04 17:53:21 +0530

    fix(init): fix potential icorrect layer initliation  on launch on fresh system

--- src/components/store.ts
+++ src/components/store.ts
@@ -2,11 +2,10 @@ import { writable } from "svelte/store";
 import { cNeuron, Layer, af_enum } from "./NN_comps/NN_classes";
 import { getPreference } from "../lib/db/localStorage";
 export let currentNeuron_store = writable<cNeuron | null>(null);
-export let hidOutLayers_store = writable<Layer[]>([]);
+export let hidOutLayers_store = writable<Layer[]>([new Layer(1, 1, false)]);
 export let hiddenLayersCount_store = writable<number>(1);
 export let hiddenLayersNeuronCount_store = writable<number[]>([1]);
 export let selActivaFn_store = writable<af_enum>(
   getPreference<af_enum>("activation_function") || af_enum.relu
 );
 export let randomisedVals_store = writable(false);
-
